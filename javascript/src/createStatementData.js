5f3e0fc1942d8fda3a861321a55459560bd2f53b
ian.haworth <137287384+ian-hmrc@users.noreply.github.com>
2024-09-30 21:39:29 +0100

    Makes the PerformanceCalculator effectively abstract

--- javascript/src/createStatementData.js
+++ javascript/src/createStatementData.js
@@ -52,8 +52,9 @@ function renderPlainText(data, plays) {
 function createPerformanceCalculator(aPerformance, aPlay) {
     switch (aPlay.type) {
         case "tragedy": return new TragedyCalculator(aPerformance, aPlay);
-        case "comedy": return new ComedyCalculator(aPerformance, aPlay);
-        default: return new PerformanceCalculator(aPerformance, aPlay);
+        case "comedy" : return new ComedyCalculator(aPerformance, aPlay);
+        default:
+            throw new Error(`unknown type: ${aPlay.type}`);
     }
 }
 
@@ -64,25 +65,7 @@ class PerformanceCalculator {
     }
 
     get amount() {
-        let result = 0;
-        switch (this.play.type) {
-            case "tragedy":
-                result = 40000;
-                if (this.performance.audience > 30) {
-                    result += 1000 * (this.performance.audience - 30);
-                }
-                break;
-            case "comedy":
-                result = 30000;
-                if (this.performance.audience > 20) {
-                    result += 10000 + 500 * (this.performance.audience - 20);
-                }
-                result += 300 * this.performance.audience;
-                break;
-            default:
-                throw new Error(`unknown type: ${this.play.type}`);
-        }
-        return result;
+        throw new Error(`subclass responsibility`);
     }
 
     get volumeCredits() {
