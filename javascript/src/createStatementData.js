84afa0ab87ef504f09278f827dfc0c21bc833d91
ian.haworth <137287384+ian-hmrc@users.noreply.github.com>
2024-09-30 20:56:51 +0100

    Adds the empty PerformanceCalculator class

--- javascript/src/createStatementData.js
+++ javascript/src/createStatementData.js
@@ -8,6 +8,7 @@ function createStatementData(invoice, plays) {
     return statementData;
 
     function enrichPerformance(aPerformance) {
+        const calculator = new PerformanceCalculator(aPerformance);
         const result = Object.assign({}, aPerformance)
         result.play = playFor(result);
         result.amount = amountFor(result);
@@ -75,6 +76,12 @@ function renderPlainText(data, plays) {
                            minimumFractionDigits: 2 }).format(aNumber/100);
     }
 
+}
+
+class PerformanceCalculator {
+    constructor(aPerformance) {
+        this.performance = aPerformance;
+    }
 }
 
 module.exports = createStatementData;
